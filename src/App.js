1338298da23e486e8b0080f7e96edef232e934be
randhavevaibhav <randhavevaibhav3@gmail.com>
2023-12-10 23:09:58 +0530

    removed console logs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// https://codesandbox.io/s/spring-flower-x2dr5n
+
 import React, { useState, useEffect,useRef } from "react";
 import {
   MapContainer,
   TileLayer,
-  Polygon,
-  Polyline,
+  
   Popup,
   Marker,
   FeatureGroup
@@ -27,7 +26,7 @@ import { twoPosContext } from "./Contexts/twoPosContext";
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-// import RoutingMachine from "./RoutingMachine";
+
 
 import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
@@ -43,7 +42,7 @@ const baseMaps = {
   OpenStreetMap: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   Satellite:
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-  // Add more base maps here...
+ 
 };
 
 
@@ -106,7 +105,6 @@ const PolygonMap = () => {
   }
 
 
-  // Routing Machine
   const createRoutineMachineLayer = (twoPos) => {
    
     let startLat=twoPos.NextPos[0][0];
@@ -114,10 +112,7 @@ const PolygonMap = () => {
     let endtLat=twoPos.NextPos[1][0];
     let endtLong=twoPos.NextPos[1][1];
    
-    console.log("startLat ===> "+startLat);
-    console.log("startLong ===> "+startLong);
-    console.log("endtLat ===> "+endtLat);
-    console.log("endtLong ===> "+endtLong);
+  
 
 
 
@@ -142,7 +137,6 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
 
 
-  // Routing Machine
 
 
 
@@ -157,18 +151,18 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
 
   function makeOverpassQuery(qstrg) {
-    // Split the input string into individual coordinates
+    
     const coordinates = qstrg.split(" ");
     coordinates.pop();
 
     console.log(coordinates);
 
-    // Ensure that we have a valid number of coordinates
+   
     if (coordinates.length % 2 !== 0) {
       throw new Error("Invalid number of coordinates in qstrg");
     }
 
-    // Construct the query dynamically based on the coordinates
+    
     const coordinatePairs = [];
     let ply1 = '';
     for (let i = 0; i < coordinates.length; i += 2) {
@@ -182,8 +176,7 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
     }
 
-    // Join the coordinate pairs to create the polygon in the query
-    // const polygon = coordinatePairs.join(" ");
+    
     ply1 = ply1.trim();
 
     console.log(ply1);
@@ -202,15 +195,14 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
     
     const { layer } = e;
-    console.log("layer ---> "+layer.toGeoJSON().geometry.coordinates[0]);
+    
     setDrawnPolygon(layer.toGeoJSON());
     const points = layer.toGeoJSON().geometry.coordinates[0];
-    console.log("points ---> "+points);
+   
     let qstrg = '';
     points.forEach(point => {
-      // console.log("Point[0] ---> "+point[0]);
-      // console.log("Point[1] ---> "+point[1]);
-      console.log("points ---> "+point);
+    
+     
       const x = point[0];
       const y = point[1];
       qstrg += y;
@@ -221,25 +213,23 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
 
     });
-    console.log("qstrg ----> "+qstrg);
+   
     const qry = makeOverpassQuery(qstrg);
-    console.log("qry ----> "+qry);
+   
     const encodedQuery = encodeURIComponent(qry);
-    console.log("encodedQuery ----> "+encodedQuery);
+    
     const overpassApiUrl = `https://overpass-api.de/api/interpreter?data=${encodedQuery}`;
 
     axios.get(overpassApiUrl)
       .then(response => {
         // Handle the response data here
-         console.log(response.data);
+        
         const restaurants = response.data.elements.filter(node => node.tags && node.tags.name);
-         console.log("restaurants  ====> "+restaurants);
+        
         response.data.elements.forEach(restaurant => {
           
           const rname = restaurant.tags.name ? restaurant.tags.name : undefined;
-          console.log("restaurant name -----> "+rname);
-          console.log("latitude -----> "+restaurant.lat);
-          console.log("logitude ------> "+restaurant.lon);
+        
 
           latlons.push([[restaurant.lat, restaurant.lon], rname]);
           
@@ -252,7 +242,7 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
           //   console.log("______");
         });
         addMarkers(latlons);
-        console.log("combined latlons ====> "+latlons);
+        
       })
       .catch(error => {
         console.log("error", error);
@@ -353,9 +343,7 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
           </option>
         ))}
       </select>
-      {/* {drawnPolygon && (
-        <p>Workers inside drawn polygon: {"tmp"}</p>
-      )} */}
+    
     </div>
     
     
@@ -368,7 +356,7 @@ const RoutingMachine = createControlComponent(createRoutineMachineLayer);
 
    
   </MapContainer>
-  {console.log(selectTwoPos)}
+  
     </twoPosContext.Provider>
     
     
